5cca298d35351f582ce8172a74fb3b03ad0c7be1
Federico Agra <ChefeIdon@users.noreply.github.com>
2017-10-10 03:22:16 -0300

    Documentacion y Actualización de Creencias
    -Se comenzo con la documentación
    -Se actualizo la parte de actualización de creencias, falta el caso en que ya no este esa entidad que creiamos que estaba en tal posición.

--- miguelito/module_beliefs_update.pl
+++ miguelito/module_beliefs_update.pl
@@ -24,44 +24,70 @@
 update_beliefs(Percepcion):-
 	forall(member(Relacion,Percepcion),actualizar(Relacion)).
 
+/*
+	Actualiza la posición de una entidad encontrada
+*/
 actualizar(atPos(Entidad,PosVector)):-
 	retractall(atPos(Entidad,_PosVectorViejo)),
 	assert(atPos(Entidad,PosVector)).
 
+/*
+	Actualiza la descripción de una entidad encontrada
+*/
 actualizar(entity_descr(Entidad,Descripcion)):-
 	retractall(entity_descr(Entidad,_DescripcionVieja)),
 	assert(entity_descr(Entidad,Descripcion)).
 
+/*
+	Actualiza la relación has de una entidad encontrada que tiene otra entidad
+*/
 actualizar(has(EntidadDuena,Entidad)):-
-	at(Entidad,_PosVieja), %Recordamos que esa entidad estaba en alguna posicion?
-	retractall(at(Entidad,_PosVieja)), %Borrar donde estaba
-	assert(has(EntidadDuena,Entidad)). %Recordar quien la tiene
+	at(Entidad,_PosVieja),				%Recordamos que esa entidad estaba en alguna posicion?
+	retractall(at(Entidad,_PosVieja)),	%Borrar donde estaba
+	assert(has(EntidadDuena,Entidad)). 	%Recordar quien la tiene
 
+/*
+	Recuerdo la ultima moneda que agarre
+*/
 actualizar(has([agent,IDAgent],[gold,IDGold])):-
+	not(has([agent,IDAgent],[gold,IDGold])),	%si no lo recorde antes
 	assert(has([agent,IDAgent],[gold,IDGold])). %Recordar que el agente posee un tesoro nuevo
 
+/*
+	Si una entidad tiene a otra, borro si creia que a esta ultima la tenia orta entidad.
+*/
 actualizar(has(EntidadDuena,Entidad)):-
-	retractall(has(EntidadDuena,_EntidadViejas)), %Borrar las entidades viejas que tenia
-	assert(has(EntidadDuena,Entidad)). %Recordar nueva entidad que posee
+	retractall(has(EntidadDuena,_EntidadViejas)),	%Borrar las entidades viejas que tenia
+	assert(has(EntidadDuena,Entidad)). 				%Recordar nueva entidad que posee
 
+/*
+	Actualiza el tiempo
+*/
 actualizar(time(T)):-
-	retractall(time(_)), %Borrar tiempo anterior
-	assert(time(T)). %Recordar tiempo actual
+	retractall(time(_)), 	%Borrar tiempo anterior
+	assert(time(T)). 		%Recordar tiempo actual
 
+/*
+	Actualiza los nodos
+*/
 actualizar(node(Id,Pos,Ad)):-
-	retractall(node(Id,_Pos,_Ad)), %Borrar previa conocimiento del nuevo nodo
-	assert(node(Id,Pos,Ad)). %Recordar nodo
+	retractall(node(Id,_Pos,_Ad)),	%Borrar previa conocimiento del nuevo nodo
+	assert(node(Id,Pos,Ad)). 		%Recordar nodo
 
-
+/*
+	Encuentro una entidad tirada en el piso y me fijo si creia que la tenia alguien mas
+*/
 actualizar(at(Entidad,PosNueva)):-
-	has(_AlgunaEntidad,Entidad), %Recordamos que esa entidad la tenia otro entidad?
-	retractall(has(_AlgunaEntidad,Entidad)), %Borrar quien la tenia
-	assert(at(Entidad,PosNueva)). %Recordar nueva ubicacion
+	has(_AlgunaEntidad,Entidad), 				%Recordamos que esa entidad la tenia otro entidad?
+	retractall(has(_AlgunaEntidad,Entidad)),	%Borrar quien la tenia
+	assert(at(Entidad,PosNueva)). 				%Recordar nueva ubicacion
 
-
+/*
+	Encuentro una entidad y actualizo el at
+*/
 actualizar(at(Entidad,PosNueva)):-
-	retractall(at(Entidad,_PosVieja)), %Borrar ubicacion vieja
-	assert(at(Entidad,PosNueva)). %Recordar nueva ubicacion
+	retractall(at(Entidad,_PosVieja)), 	%Borrar ubicacion vieja
+	assert(at(Entidad,PosNueva)).		%Recordar nueva ubicacion
 
 
 
